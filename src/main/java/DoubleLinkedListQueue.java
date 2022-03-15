1d4f58110b01b485d440b692879640a5303c64a6
pablogodiaz <pablogodiaz+1@gmail.com>
2022-03-15 12:05:47 +0100

    Added appendLeft in DoubleLinkedListQueue

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -6,15 +6,20 @@ public class DoubleLinkedListQueue implements DoubleEndedQueue<T> {
         this.list = null;
     }
 
-    public append(DequeNode<T> node) {
+    public void append(DequeNode<T> node) {
         if(list == null) {
             list = node;
         } else {
-            DequeNode<T> temp = list;
+            DequeNode<T> temp = this.list;
             while(temp.next != null) {
                 temp = temp.next;
             }
             temp.next = node;
         }
+    }
+
+    public void appendLeft(DequeNode<T> node) {
+        node.next = this.list;
+        this.list = node;
     }
 }
