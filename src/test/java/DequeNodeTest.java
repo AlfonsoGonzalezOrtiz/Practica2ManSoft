c3d755448d6efa88c66f994cf05c9f7c6e053c00
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-03-15 11:57:25 +0100

    TestCase DequeNodeNull

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,2 +1,28 @@
-public class DequeNodeTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test cases:
+ * factorial 0 -> 1 *
+ * factorial 1 -> 1
+ * factorial 2 -> 2
+ * factorial 3 -> 6
+ * factorial 6 -> 720
+ */
+class DequeNodeTest {
+
+    private DequeNode dnode;
+
+
+    @Test
+    public void DequeNodeNull() {
+        dnode = new DequeNode(null,null,null);
+        assertEquals(dnode.getItem(),null);
+        assertEquals(dnode.getNext(),null);
+        assertEquals(dnode.getNext(),null);
+    }
+
 }
