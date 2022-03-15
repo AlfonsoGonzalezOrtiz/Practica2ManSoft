53947e763741f86ed4559a8c8dab01c2f9804148
pablogodiaz <pablogodiaz+1@gmail.com>
2022-03-15 12:01:47 +0100

    Added append in DoubleLinkedListQueue

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -5,4 +5,16 @@ public class DoubleLinkedListQueue implements DoubleEndedQueue<T> {
     public DoubleLinkedListQueue() {
         this.list = null;
     }
+
+    public append(DequeNode<T> node) {
+        if(list == null) {
+            list = node;
+        } else {
+            DequeNode<T> temp = list;
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
 }
