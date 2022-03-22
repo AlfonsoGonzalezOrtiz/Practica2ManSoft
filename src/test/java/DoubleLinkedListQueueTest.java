e7217755f69c589b4009e01601267c4bf9d1dd7e
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-03-22 12:20:33 +0100

    Append TestCases and DeleteFirst TestCases

--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -19,58 +19,34 @@ public class DoubleLinkedListQueueTest {
     }
 
     @Test
-    public void PeekFirstOneNode(){
-
-    }
-
-    @Test
-    public void PeekFirstMultipleNodes(){
-
-    }
-
-    @Test
     public void PeekLastNodeless(){
-
-    }
-
-    @Test
-    public void PeekLastOneNode(){
-
-    }
-
-    @Test
-    public void PeekLastMultipleNodes(){
-
+        assertNull(list.peekLast());
     }
 
     @Test
     public void SizeNodeless(){
-
+        assertEquals(0,list.size());
     }
 
-    @Test
-    public void SizeOneNode(){
-
-    }
-
-    @Test
-    public void SizeMultipleNodes(){
-
-    }
 
     @Test
     public void AppendNodeless(){
-
+        DequeNode node = new DequeNode(1,null,null);
+        list.append(node);
+        assertEquals(node,list.peekFirst());
+        assertEquals(node,list.peekLast());
+        assertEquals(1,list.size());
     }
 
     @Test
     public void AppendOneNode(){
-
-    }
-
-    @Test
-    public void AppendMultipleNodes(){
-
+        DequeNode node = new DequeNode(1,null,null);
+        DequeNode node2 = new DequeNode(2,null,null);
+        list.append(node);
+        list.append(node2);
+        assertEquals(node,list.peekFirst());
+        assertEquals(node,list.peekLast());
+        assertEquals(2,list.size());
     }
 
     @Test
@@ -83,24 +59,35 @@ public class DoubleLinkedListQueueTest {
 
     }
 
-    @Test
-    public void AppendLeftMultipleNodes(){
-
-    }
 
     @Test
     public void DeleteFirstNodeless(){
-
+        list.deleteFirst();
+        assertNull(list.peekFirst());
+        assertNull(list.peekLast());
+        assertEquals(0,list.size());
     }
 
     @Test
     public void DeleteFirstOneNode(){
-
+        DequeNode node = new DequeNode(1,null,null);
+        list.append(node);
+        list.deleteFirst();
+        assertNull(list.peekFirst());
+        assertNull(list.peekLast());
+        assertEquals(0,list.size());
     }
 
     @Test
-    public void DeleteFistMultipleNodes(){
-
+    public void DeleteFirstMultipleNodes(){
+        DequeNode node = new DequeNode(1,null,null);
+        DequeNode node2 = new DequeNode(2,null,null);
+        list.append(node);
+        list.append(node2);
+        list.deleteFirst();
+        assertEquals(node2,list.peekFirst());
+        assertEquals(node2,list.peekLast());
+        assertEquals(1,list.size());
     }
 
     @Test
@@ -112,11 +99,5 @@ public class DoubleLinkedListQueueTest {
     public void DeleteLastOneNode(){
 
     }
-
-    @Test
-    public void DeleteLastMultipleNodes(){
-
-    }
-
 
 }
