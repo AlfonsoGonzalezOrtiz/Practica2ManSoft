963d6301ed0d3a55f195c5e32bd6cd515e09ea2b
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-03-22 11:53:04 +0100

    TestCases Skeleton

--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -1,16 +1,122 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class DoubleLinkedListQueueTest {
 
-    private DequeNode node;
+    private DoubleLinkedListQueue list;
+
+    @BeforeEach
+    public void init(){
+        list = new DoubleLinkedListQueue<>();
+    }
 
     @Test
-    public void hasOneNode() {
-        node = new DequeNode(1, null, null);
-        DequeNode expectedPrevious = null;
-        DequeNode expectedNext = null;
-        boolean expectedFirst = true;
-        boolean expectedLast = true;
+    public void PeekFirstNodeless() {
+        assertNull(list.peekFirst());
     }
+
+    @Test
+    public void PeekFirstOneNode(){
+
+    }
+
+    @Test
+    public void PeekFirstMultipleNodes(){
+
+    }
+
+    @Test
+    public void PeekLastNodeless(){
+
+    }
+
+    @Test
+    public void PeekLastOneNode(){
+
+    }
+
+    @Test
+    public void PeekLastMultipleNodes(){
+
+    }
+
+    @Test
+    public void SizeNodeless(){
+
+    }
+
+    @Test
+    public void SizeOneNode(){
+
+    }
+
+    @Test
+    public void SizeMultipleNodes(){
+
+    }
+
+    @Test
+    public void AppendNodeless(){
+
+    }
+
+    @Test
+    public void AppendOneNode(){
+
+    }
+
+    @Test
+    public void AppendMultipleNodes(){
+
+    }
+
+    @Test
+    public void AppendLeftNodeless(){
+
+    }
+
+    @Test
+    public void AppendLeftOneNode(){
+
+    }
+
+    @Test
+    public void AppendLeftMultipleNodes(){
+
+    }
+
+    @Test
+    public void DeleteFirstNodeless(){
+
+    }
+
+    @Test
+    public void DeleteFirstOneNode(){
+
+    }
+
+    @Test
+    public void DeleteFistMultipleNodes(){
+
+    }
+
+    @Test
+    public void DeleteLastNodeless(){
+
+    }
+
+    @Test
+    public void DeleteLastOneNode(){
+
+    }
+
+    @Test
+    public void DeleteLastMultipleNodes(){
+
+    }
+
+
 }
