11ad46340acdc49548ef273b983faf2f9da04139
pablogodiaz <pablogodiaz+1@gmail.com>
2022-03-24 13:08:31 +0100

    Added comments on all methods

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -8,6 +8,11 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         this.last = null;
     }
 
+    /**
+     * Adds the node parameter at the end of the list.
+     *
+     * @param node
+     */
     public void append(DequeNode<T> node) {
         if(first == null){
             first = node;
@@ -23,6 +28,11 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         }
     }
 
+    /**
+     * Adds the node parameter at the beginning of the list.
+     *
+     * @param node
+     */
     public void appendLeft(DequeNode<T> node) {
         if(first == null){
             first = node;
@@ -38,6 +48,10 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         }
     }
 
+    /**
+     * Deletes the first node of the list.
+     *
+     */
     public void deleteFirst() {
         if(first != null){
             if (first.equals(last)){
@@ -56,6 +70,10 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         }
     }
 
+    /**
+     * Deletes the last node of the list.
+     *
+     */
     public void deleteLast() {
         if(first != null){
             if(first.equals(last)){
@@ -76,19 +94,35 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         }
     }
 
+    /**
+     * Returns the first element of the list. Returns null if the list is empty.
+     *
+     * @return first
+     */
     public DequeNode<T> peekFirst() {
         return first;
     }
 
+    /**
+     * Returns the last element of the list. Returns null if the list is empty.
+     *
+     * @return last
+     */
     public DequeNode<T> peekLast() {
         return last;
     }
 
+    /**
+     * Returns the node at position i, starting with 1, ending with size().
+     * If i is out of bounds for the list, null is returned.
+     *
+     * @param i
+     */
     public DequeNode<T> getAt(int i) {
         DequeNode<T> result = null;
         if(i > 0 && i <= this.size()) {
             int cnt = 1;
-            result = this.list;
+            result = this.first;
             while(cnt < i) {
                 result = result.getNext();
                 ++cnt;
@@ -97,6 +131,11 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         return result;
     }
 
+    /**
+     * Returns the size of the list. Returns 0 if the list is empty.
+     *
+     * @return size
+     */
     public int size() {
         DequeNode<T> temp = first;
         int size = 0;
